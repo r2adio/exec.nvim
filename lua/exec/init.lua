7e926c7d8787c0208ddbfec8f938815b35505941
gauravashisth <miya.musashi.moto1645@gmail.com>
2026-03-04 21:00:11 +0530

    feat: retuns a list of sub-commmands

--- lua/exec/init.lua
+++ lua/exec/init.lua
@@ -1,15 +1,17 @@
 local M = {}
 
 M.config = {
-	message = "📌 exec.nvim loaded!",
+	-- message = "📌 exec.nvim loaded!",
 }
 
 function M.setup(opts)
 	M.config = vim.tbl_deep_extend("force", M.config, opts or {})
 end
 
-function M.welcome()
-	vim.notify(M.config.message, vim.log.levels.INFO, { title = "exec.nvim" })
+function M.out_subcmds(opts)
+	local cmds = opts.fargs
+	-- print(vim.inspect(cmds))
+	return cmds
 end
 
 return M
--- plugin/exec.lua
+++ plugin/exec.lua
@@ -3,6 +3,11 @@ if vim.g.loaded_exec_nvim then
 end
 vim.g.loaded_exec_nvim = true
 
-vim.api.nvim_create_user_command("X", function()
-	require("exec").welcome()
-end, {})
+vim.api.nvim_create_user_command("X", function(opts)
+	require("exec").out_subcmds(opts)
+end, {
+	nargs = "*",
+	complete = function()
+		return { "make", "other_subcmd" }
+	end,
+})
